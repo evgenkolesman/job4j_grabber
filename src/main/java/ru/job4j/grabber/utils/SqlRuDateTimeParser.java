bb381b7946568f1508fe45b0b3b33306cbff0951
glavs <glavstroi_e@mail.ru>
2021-06-22 10:20:49 +0300

    2.1. Преобразование даты [#289476]

--- src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
+++ src/main/java/ru/job4j/grabber/utils/SqlRuDateTimeParser.java
@@ -1,30 +1,12 @@
 package ru.job4j.grabber.utils;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-
-import java.io.IOException;
 import java.time.LocalDateTime;
 
-import static ru.job4j.quartz.AlertRabbit.init;
 
 public class SqlRuDateTimeParser implements DateTimeParser {
 
     @Override
     public LocalDateTime parse(String parse) {
-        try {
-            init();
-            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-            Elements row = doc.select(".postslisttopic");
-            for (Element a : row) {
-                Element href1 = a.parent().child(5);
-                parse = href1.data();
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
         return LocalDateTime.parse(parse);
     }
 }
