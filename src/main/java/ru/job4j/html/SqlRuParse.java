f131bf3136cc31bf43de88a387fabcab16573165
glavs <glavstroi_e@mail.ru>
2021-06-22 11:15:54 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210 #169563] тут работает

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,13 +5,14 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 /*
  * Парсинг html страниц
  * выводим данные
  * по классу
  * @class .postslisttopic
- * и даты в довесок
+ * даты, выборка по страницам
  * @author Kolesnikov Evgeniy (evgeniysanich@mail.ru)
  * @version 1.0
  */
@@ -21,12 +22,18 @@ public class SqlRuParse {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
 
-        for (Element td : row) {
-            Element href = td.child(0);
-            System.out.println(href.attr("href"));
-            System.out.println(href.text());
-            Element href1 = td.parent().child(5);
-            System.out.println(href1.text());
+        for (int i = 1; i < 6; i++) {
+            for (Element td : row) {
+                Element href = td.child(0);
+                System.out.println(href.attr("href"));
+                String vac = href.text();
+                Element href1 = td.parent().child(5);
+                String dat = href1.text();
+                //SqlRuDateTimeParser a = new SqlRuDateTimeParser();
+                String url = "https://www.sql.ru/forum/job-offers";
+                String link = String.format("%s/%d%n%s%n%s%n", url, i, vac, dat);
+                System.out.println(link);
+            }
         }
     }
 }
