3b756ca7a524ea0f1a917fdacd8cdacac5c556e0
glavs <glavstroi_e@mail.ru>
2021-06-22 16:53:59 +0300

    Исправил замечания

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -19,10 +19,11 @@ import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 public class SqlRuParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
-        Elements row = doc.select(".postslisttopic");
-
+        /*Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");*/
         for (int i = 1; i < 6; i++) {
+            Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+            Elements row = doc.select(".postslisttopic");
             for (Element td : row) {
                 Element href = td.child(0);
                 System.out.println(href.attr("href"));
