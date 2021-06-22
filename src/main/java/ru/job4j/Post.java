dc5716f10f9fbb9da0351e5b8fe25ff11562e3a2
glavs <glavstroi_e@mail.ru>
2021-06-22 17:31:48 +0300

    2.2. Модель данных - Post. [#285211]

--- src/main/java/ru/job4j/Post.java
+++ src/main/java/ru/job4j/Post.java
@@ -1,14 +1,16 @@
 package ru.job4j;
 
+import java.time.LocalDateTime;
+
 public class Post {
 
     private final String id;
     private final String messageHeader;
     private final String msgTable;
     private final String link;
-    private final String created_data;
+    private final LocalDateTime created_data;
 
-    public Post(String id, String messageHeader, String msgTable, String link, String created_data) {
+    public Post(String id, String messageHeader, String msgTable, String link, LocalDateTime created_data) {
         this.id = id;
         this.messageHeader = messageHeader;
         this.msgTable = msgTable;
