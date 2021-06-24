022ecec8d4dfeccd9443152e3db9c2d9cb097a79
glavs <glavstroi_e@mail.ru>
2021-06-24 14:42:18 +0300

    2.4. SqlRuParse [#285213 #170849]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -5,8 +5,12 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.Parse;
 import ru.job4j.Post;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /*
  * Парсинг html страниц
@@ -18,7 +22,7 @@ import ru.job4j.grabber.utils.SqlRuDateTimeParser;
  * @version 1.0
  */
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
     public static void main(String[] args) throws Exception {
         for (int i = 1; i < 6; i++) {
             Document doc = Jsoup.connect(String.format("https://www.sql.ru/forum/job-offers/%s", i)).get();
@@ -32,18 +36,48 @@ public class SqlRuParse {
                 SqlRuDateTimeParser a = new SqlRuDateTimeParser();
                 String url = String.format("https://www.sql.ru/forum/job-offers/%s", i);
                 String link = String.format("%s%n%s%n%s%n", url, vac, a.parse(dat));
-
                 /*Post post = GetParse.getParse(url);
                 System.out.println(post.toString()); тут не работает*/
                 System.out.println(link);
+                SqlRuParse e1 = new SqlRuParse();
+                System.out.println(e1.detail(url));
             }
         }
         String url1 = "https://www.sql.ru/forum/1325330/lidy-be-fe-senior-cistemnye-analitiki-qa-i-devops-moskva-do-200t";
         Post post = GetParse.getParse(url1);
-        System.out.println(post.toString());
+        SqlRuParse e1 = new SqlRuParse();
+        System.out.println(e1.list(url1));
     }
 
+    @Override
+    public List<Post> list(String link) {
+        List<Post> listPost = new ArrayList<>();
+        // тут мы получаем url и по нему готовим список постов, с помощью detail
+        listPost.add(detail(link));
+        return listPost;
+    }
 
+    @Override
+    public Post detail(String link) {
+        //получаем ссылку и с нее выводим модель POST
+        try {
+            Document page = Jsoup.connect(link).get();
+            Element page1 = page.select("table[class=msgTable]").first();
+            Element name = page1.select("td[class=messageHeader]").first();
+            String name1 = name.text();
+            String id = name.attr("id");
+            Element date = page1.select("td[class=msgFooter]").first();
+            String date1 = date.text().split(" \\[")[0];
+            Element context = page1.select("td[class=msgBody]").last();
+            String context1 = context.text();
+            SqlRuDateTimeParser dateD = new SqlRuDateTimeParser();
+            String link1 = page1.select(("td[class=msgBody]")).first().text();
+            return new Post(id, name1, context1, link1, dateD.parse(date1));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
 
 
