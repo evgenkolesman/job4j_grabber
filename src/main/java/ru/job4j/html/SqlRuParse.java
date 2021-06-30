bc464bf1be696d4ed88582ad001a29907d726328
glavs <glavstroi_e@mail.ru>
2021-06-30 12:22:26 +0300

    6. Grabber. [#289477] Пока пробная версия мониторинга

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -6,7 +6,6 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.Parse;
-import ru.job4j.Post;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
